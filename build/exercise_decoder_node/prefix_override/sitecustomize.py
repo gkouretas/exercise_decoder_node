0293d93e181c5faf756bf24dd53296e625fc33ac
gkouretas <thegreekgeorge07@gmail.com>
2024-12-12 21:46:06 -0800

    whoops

--- build/exercise_decoder_node/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/georgekouretas/ros2_ws/src/exercise_decoder_node/install/exercise_decoder_node'
--- build/exercise_decoder_node/share/exercise_decoder_node/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/georgekouretas/ros2_ws/src/exercise_decoder_node/build/exercise_decoder_node"
